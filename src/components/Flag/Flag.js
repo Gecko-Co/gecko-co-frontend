d29a6ff129d9ae8fd0be0fa2d27c923571bc08e8
Jan Carlo Once <jancarloonce11@gmail.com>
2024-04-13 14:05:55 +0800

    rename env var to FLAG

--- src/components/Flag/Flag.js
+++ src/components/Flag/Flag.js
@@ -4,7 +4,7 @@ import './Flag.scss';
 const Flag = () => {
     const [isFloating, setIsFloating] = useState(false);
     console.log("ENV var: ",process.env.FLAG)
-    const environment = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' ? 'DEV' : 'PROD';
+    const environment = process.env.FLAG === 'preview' ? 'DEV' : 'PROD';
     const environmentStyle = environment === 'DEV' ? { color: 'blue', fontWeight: 'bold' } : { color: 'red', fontWeight: 'bold' };
 
     useEffect(() => {
