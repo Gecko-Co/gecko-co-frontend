f79ccf630b0ad3b61b54f2f8da8e82f5183240da
Jan Carlo Once <jancarloonce11@gmail.com>
2024-04-13 15:03:54 +0800

    Add button for clear filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -12,24 +12,39 @@ const Filter = ({
 }) => {
   const [selectedStatus, setSelectedStatus] = useState('');
   const [selectedSpecies, setSelectedSpecies] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
 
   let clear = () => {
     updateStatus("");
     updateGender("");
     updateSpecies("");
     updatePageNumber(1);
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 200);
   };
 
   return (
     <div className="col-lg-3 col-12 mb-5">
       <div className="text-center fw-bold fs-4 mb-2">Filters</div>
-      <div
-        style={{ cursor: "pointer" }}
+      <button
+        style={{ 
+          cursor: "pointer", 
+          backgroundColor: isClicked ? "#0b5ed7" : "#23283b", 
+          color: "white", 
+          border: "none", 
+          padding: "5px 20px", 
+          borderRadius: "4px", 
+          transition: "background-color 0.3s ease",
+          display: "block",
+          margin: "0 auto"
+        }}
         onClick={clear}
-        className="text-primary text-decoration-underline text-center mb-3"
+        className="text-decoration-none text-center mb-3"
       >
-        Clear Filters
-      </div>
+        {isClicked ? 'Clearing...' : 'Clear Filters'}
+      </button>
       <div className="accordion" id="accordionExample">
         <Status
           selectedStatus={selectedStatus}
@@ -48,11 +63,18 @@ const Filter = ({
           updateGender={updateGender}
         />
       </div>
-      {/* Inline styles for the open accordion heading */}
+      {/* Inline styles for the open accordion heading and Clear Filters button */}
       <style jsx>{`
         .accordion-button:not(.collapsed) {
-          background-color: #23283b; /* Same as footer background color */
+          background-color: #23283b !important; /* Same as footer background color */
           color: white !important; /* Important to override Bootstrap's default color */
+        }
+        .accordion-button:not(.collapsed):hover {
+          background-color: #23283b !important; /* Same as selected background color */
+          color: white !important; /* Important to override Bootstrap's default color */
+        }
+        button:hover {
+          background-color: #0b5ed7;
         }
       `}</style>
     </div>
