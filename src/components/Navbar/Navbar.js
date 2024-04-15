68ffc1487ec9a3c0b302fd39acd0f35fe4aacfd2
Jan Carlo Once <jancarloonce11@gmail.com>
2024-04-15 20:51:25 +0800

    Change maintenance icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -63,7 +63,7 @@ const NavLink = ({ to, text, toggleMenu }) => {
         {text}
       </Link>
       {shouldShowBadge(text) && (
-        <span className="maintenance-icon">&#x1F6A8;</span>
+        <span className="maintenance-icon">&#x1F6A7;</span>
       )}
     </div>
  );
