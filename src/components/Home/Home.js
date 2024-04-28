db06bd776673c15321261eb484017a81dc527b50
Jan Carlo Once <jancarloonce11@gmail.com>
2024-04-28 14:20:37 +0800

    Added homepage responsiveness fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,22 +5,33 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     const typedRef = useRef(null);
-    const [currentImageIndex, setCurrentImageIndex] = useState(0); // State to manage the current image index
-    const images = [ // Array of image sources
-        "images/home1.png",
-        "images/home2.png",
-        "images/home3.png",
-        // Add more image sources as needed
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const images = [
+        "images/home1-resize.png",
+        "images/home2-resize.png",
+        "images/home3-resize.png",
     ];
+
+    const words = ["love?"];
+    const [changingWordIndex, setChangingWordIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setChangingWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+        }, 3000); // Change word every 3 seconds
+
+        return () => clearInterval(interval);
+    }, [words]);
 
     useEffect(() => {
         const options = {
-            strings: ["Looking for the next pet you would surely love?"],
+            strings: [words[changingWordIndex]], // Use the current word from the array
             typeSpeed: 40,
             backSpeed: 50,
             loop: true,
             contentType: 'text',
             cursorChar: '',
+            smartBackspace: true, // Enable smart backspacing
         };
 
         const typed = new Typed(typedRef.current, options);
@@ -28,33 +39,34 @@ function Home() {
         return () => {
             typed.destroy();
         };
-    }, []);
+    }, [changingWordIndex]); // Depend on the changing word index to reset the Typed.js instance
 
-    // Function to update the current image index based on the clicked dot
     const goToSlide = (index) => {
         setCurrentImageIndex(index);
     };
 
-    // Set up an interval to change the image every 5 seconds
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000); // Change image every 5 seconds
+        }, 3000);
 
-        return () => clearInterval(interval); // Clean up the interval on component unmount
-    }, [images.length]); // Depend on the length of the images array to reset the interval if images change
+        return () => clearInterval(interval);
+    }, [images.length]);
 
     return (
         <div className="App">
             <div className="first-section">
                 <div className="text-container">
-                    <h1 className="text-center mb-3" ref={typedRef}></h1>
+                    <h1 className="text-center mb-3">
+                        Looking for a new pet you will </h1>
+                        <h1 className="text-center mb-3"> surely <span className="red-text" ref={typedRef}></span>
+                    </h1>
                     <Link to="/shop" className="shop-now-button">SHOP NOW!</Link>
                 </div>
                 <div className="image-container">
                     <img src={images[currentImageIndex]} alt="Background" className="background-image" />
                 </div>
-                <div className="slideshowDots">
+                {/* <div className="slideshowDots">
                     {images.map((_, index) => (
                         <div
                             key={index}
@@ -62,7 +74,7 @@ function Home() {
                             onClick={() => goToSlide(index)}
                         ></div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
     );
