aa354a9484fa494f30079007ff35b9ba36a90791
Jan Carlo Once <jancarloonce11@gmail.com>
2024-06-16 23:07:30 +0800

    Fix Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,57 +1,140 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import './Footer.scss';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import styled from 'styled-components';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <a href="https://www.facebook.com/geckoco11" target="_blank" rel="noopener noreferrer" className="social-icon">
-              <div className="social-circle">
+    <StyledFooter>
+      <Container>
+        <FooterTop>
+          <FooterSection>
+            <FooterHeading>Connect with Us</FooterHeading>
+            <SocialIcons>
+              <SocialLink href="https://www.facebook.com/geckoco11" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </div>
-            </a>
-            <a href="https://www.instagram.com/geckoco11" target="_blank" rel="noopener noreferrer" className="social-icon">
-              <div className="social-circle">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </div>
-            </a>
-            <a href="https://www.twitter.com/geckoco11" target="_blank" rel="noopener noreferrer" className="social-icon">
-              <div className="social-circle">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </div>
-            </a>
-            <a href="https://www.youtube.com/geckoco11" target="_blank" rel="noopener noreferrer" className="social-icon">
-              <div className="social-circle">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-              </div>
-            </a>
-          </div>
-        </div>
-        <div className="row mt-3">
-          <div className="col-md-12 text-center">
-            <p>
-              <a href="/" className="footer-link">Home</a>
-              <span className="mx-2">|</span>
-              <a href="/shop" className="footer-link">Shop</a>
-              <span className="mx-2">|</span>
-              <a href="/" className="footer-link">Learn</a>
-              <span className="mx-2">|</span>
-              <a href="/" className="footer-link">Contact Us</a>
-            </p>
-          </div>
-        </div>
-        <div className="row mt-3">
-          <div className="col-md-12 text-center">
-            <p>© 2018-2024 Gecko Co.®</p>
-          </div>
-        </div>
-      </div>
-    </footer>
+              </SocialLink>
+            </SocialIcons>
+          </FooterSection>
+          <FooterSection>
+            <FooterHeading>Explore</FooterHeading>
+            <ExploreLinks>
+              <FooterLink href="/">Home</FooterLink>
+              <FooterLink href="/shop">Shop</FooterLink>
+              <FooterLink href="/learn">Learn</FooterLink>
+              <FooterLink href="/contact">Contact Us</FooterLink>
+            </ExploreLinks>
+          </FooterSection>
+          <FooterSection>
+            <FooterHeading>Contact Information</FooterHeading>
+            <FooterText>
+              Tandang Sora
+              <br />
+              Quezon City, Philippines
+              <br />
+              Phone: +639 274677321
+            </FooterText>
+          </FooterSection>
+          <FooterSection>
+          <FooterLogo src="/images/geckoco-png.png" alt="Geckoco Logo" />
+          </FooterSection>
+        </FooterTop>
+        <FooterBottom>
+          <FooterText>
+            &copy; 2018-2024 Gecko Co.&reg; | All Rights Reserved
+          </FooterText>
+        </FooterBottom>
+      </Container>
+    </StyledFooter>
   );
 };
+
+const FooterLogo = styled.img`
+  width: 150px;
+  height: 150px;
+  margin-right: 10px; // Adjust margin as needed
+`;
+
+const CompanyName = styled.span`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #bd692d;
+`;
+const StyledFooter = styled.footer`
+  background-color: #23283b;
+  color: white;
+  padding: 40px 0;
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+const FooterTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const FooterBottom = styled.div`
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const FooterSection = styled.div`
+  flex: 1 0 250px;
+  margin-bottom: 20px;
+`;
+
+const FooterHeading = styled.h3`
+  color: #ffffff;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const SocialLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const SocialLink = styled.a`
+  color: white;
+  text-decoration: none;
+  transition: color 0.3s;
+
+  &:hover {
+    color: orange;
+  }
+`;
+
+const ExploreLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const FooterLink = styled.a`
+  color: white;
+  text-decoration: none;
+  margin-bottom: 5px;
+  transition: color 0.3s;
+
+  &:hover {
+    color: orange;
+  }
+`;
+
+const FooterText = styled.p`
+  font-size: 14px;
+  color: #6c757d;
+`;
 
 export default Footer;
