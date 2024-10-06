f470c0caeafb914f3ff4d16f41ee3883599e7061
Jan Carlo Once <jancarloonce11@gmail.com>
2024-10-06 20:21:35 +0800

    new chat widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { CartProvider } from './components/Cart/CartContext';
+import { CartProvider } from './components/Cart/CartContext';
 import { Toaster } from 'react-hot-toast';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
@@ -27,13 +27,14 @@ inject();
 function App() {
   return (
     <Router>
-      {/* <CartProvider> */}
+      <CartProvider>
         <div className="App">
           <Flag />
           <Navbar />
           <Scroll />
           <FacebookMessenger 
         pageId="115629306499727"
+        appId="1762296367911953"
       />
         </div>
         <Routes>
@@ -58,7 +59,7 @@ function App() {
             },
           }}
         />
-      {/* </CartProvider> */}
+      </CartProvider>
     </Router>
   );
 }
--- src/components/Messenger/FacebookMessenger.js
+++ src/components/Messenger/FacebookMessenger.js
@@ -1,44 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
 
-const FacebookMessenger = ({ pageId }) => {
-  useEffect(() => {
-    // Load the Facebook SDK
-    window.fbAsyncInit = function() {
-      window.FB.init({
-        xfbml: true,
-        version: 'v16.0'
-      });
-    };
-
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    // Cleanup function
-    return () => {
-      const facebookScript = document.getElementById('facebook-jssdk');
-      if (facebookScript) {
-        facebookScript.remove();
-      }
-    };
-  }, []);
-
+const FacebookMessenger = ({ pageId, appId }) => {
   return (
-    <>
-      <div id="fb-root"></div>
-      <div 
-        className="fb-customerchat" 
-        attribution="setup_tool" 
-        page_id={pageId}
-        theme_color="#0084ff"
-        logged_in_greeting="Hi! How can we help you?"
-        logged_out_greeting="Hi! How can we help you?"
-      ></div>
-    </>
+    <FacebookProvider appId={appId}>
+      <CustomChat pageId={pageId} minimized={true} />
+    </FacebookProvider>
   );
 };
 
