708a70d7435e7b7968120a60d439f3e968dc47d0
Jan Carlo Once <jancarloonce11@gmail.com>
2024-10-07 16:59:50 +0800

    refined toast style and position

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,83 +1,31 @@
-// src/utils/toast.js
-import { toast } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 const customToast = {
   success: (message) => {
     toast.success(message, {
-      style: {
-        background: '#23283b',
-        color: '#fff',
-        zIndex: 9999999,
-      },
-      iconTheme: {
-        primary: '#bd692d',
-        secondary: '#fff',
-      },
+      position: "top-right",
+      duration: 3000,
     });
   },
   error: (message) => {
     toast.error(message, {
-      style: {
-        background: '#23283b',
-        color: '#fff',
-        zIndex: 9999999,
-      },
-      iconTheme: {
-        primary: '#bd692d',
-        secondary: '#fff',
-      },
-    });
-  },
-  info: (message) => {
-    toast(message, {
-      style: {
-        background: '#23283b',
-        color: '#fff',
-        zIndex: 9999999,
-      },
-      iconTheme: {
-        primary: '#bd692d',
-        secondary: '#fff',
-      },
+      position: "top-right",
+      duration: 3000,
     });
   },
   warning: (message) => {
     toast(message, {
       icon: '⚠️',
-      style: {
-        background: '#23283b',
-        color: '#fff',
-        zIndex: 9999999,
-      },
-      iconTheme: {
-        primary: '#bd692d',
-        secondary: '#fff',
-      },
+      position: "top-right",
+      duration: 3000,
     });
   },
-  promise: (promise, messages) => {
-    return toast.promise(
-      promise,
-      {
-        loading: messages.loading,
-        success: messages.success,
-        error: messages.error,
-      },
-      {
-        style: {
-          background: '#23283b',
-          color: '#fff',
-          zIndex: 9999999,
-        },
-        iconTheme: {
-          primary: '#bd692d',
-          secondary: '#fff',
-        },
-      }
-    );
-  },
-  dismiss: () => {
-    toast.dismiss();
+  info: (message) => {
+    toast(message, {
+      icon: 'ℹ️',
+      position: "top-right",
+      duration: 3000,
+    });
   },
 };
 
