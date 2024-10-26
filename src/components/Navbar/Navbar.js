130806304de7729d486b6c9bbd60aa65c92a0d8d
Jan Carlo Once <jancarloonce11@gmail.com>
2024-10-26 21:55:45 +0800

    Fix responsiveness issues

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -59,49 +59,52 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`navbar ${isScrolled ? 'scrolled' : 'unscrolled'} ${isOpen ? 'open' : ''}`}>
-      <Link to="/" className="logo-link">
-        <img src="/images/geckoco-png.png" alt="Geckoco Logo" className="logo" />
-        <span className="company-name">Gecko Co.</span>
-      </Link>
-      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <NavLink to="/" text="Home" toggleMenu={toggleMenu} />
-        <NavLink to="/shop" text="Shop" toggleMenu={toggleMenu} />
-        <NavLink to="/learn" text="Learn" toggleMenu={toggleMenu} />
-        <NavLink to="/blogs" text="Blogs" toggleMenu={toggleMenu} />
-        <div className={`nav-link-container tools-dropdown ${isToolsDropdownOpen ? 'open' : ''}`}>
-          <button className="nav-link" onClick={toggleToolsDropdown}>
-            Tools <FaChevronDown className={`dropdown-icon ${isToolsDropdownOpen ? 'open' : ''}`} />
-          </button>
-          <div className="tools-dropdown-menu">
-            <NavLink to="/genetic-calculator" text="Calculators" toggleMenu={toggleMenu} />
-            <NavLink to="/breeder-map" text="Breeder Map" toggleMenu={toggleMenu} />
+    <>
+      <nav className={`navbar ${isScrolled ? 'scrolled' : 'unscrolled'} ${isOpen ? 'open' : ''}`}>
+        <Link to="/" className="logo-link">
+          <img src="/images/geckoco-png.png" alt="Geckoco Logo" className="logo" />
+          <span className="company-name">Gecko Co.</span>
+        </Link>
+        <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+          <NavLink to="/" text="Home" toggleMenu={toggleMenu} />
+          <NavLink to="/shop" text="Shop" toggleMenu={toggleMenu} />
+          <NavLink to="/learn" text="Learn" toggleMenu={toggleMenu} />
+          <NavLink to="/blogs" text="Blogs" toggleMenu={toggleMenu} />
+          <div className={`nav-link-container tools-dropdown ${isToolsDropdownOpen ? 'open' : ''}`}>
+            <button className="nav-link" onClick={toggleToolsDropdown}>
+              Tools <FaChevronDown className={`dropdown-icon ${isToolsDropdownOpen ? 'open' : ''}`} />
+            </button>
+            <div className="tools-dropdown-menu">
+              <NavLink to="/genetic-calculator" text="Calculators" toggleMenu={toggleMenu} />
+              <NavLink to="/breeder-map" text="Breeder Map" toggleMenu={toggleMenu} />
+            </div>
           </div>
+          <NavLink to="/contact" text="Contact Us" toggleMenu={toggleMenu} />
         </div>
-        <NavLink to="/contact" text="Contact Us" toggleMenu={toggleMenu} />
-      </div>
-      <div className="menu-icons-container">
-        <div className="nav-icons">
-          <button onClick={handleAccountClick} className="nav-icon-link">
-            {currentUser ? (
-              <FaUserCircle className="icon" aria-label="Account" />
-            ) : (
-              <FaUserPlus className="icon" aria-label="Sign In" />
-            )}
+        <div className="menu-icons-container">
+          <div className="nav-icons">
+            <button onClick={handleAccountClick} className="nav-icon-link">
+              {currentUser ? (
+                <FaUserCircle className="icon" aria-label="Account" />
+              ) : (
+                <FaUserPlus className="icon" aria-label="Sign In" />
+              )}
+            </button>
+            <Link to="/cart" className="nav-icon-link cart-icon">
+              <FaShoppingCart className="icon" aria-label="Cart" />
+              {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
+            </Link>
+          </div>
+          <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Toggle menu">
+            <span></span>
+            <span></span>
+            <span></span>
           </button>
-          <Link to="/cart" className="nav-icon-link cart-icon">
-            <FaShoppingCart className="icon" aria-label="Cart" />
-            {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
-          </Link>
         </div>
-        <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Toggle menu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
-      {showLoginPopup && <LoginPopup onClose={() => setShowLoginPopup(false)} />}
-    </nav>
+        {showLoginPopup && <LoginPopup onClose={() => setShowLoginPopup(false)} />}
+      </nav>
+      {isOpen && <div className={`navbar-overlay ${isOpen ? 'open' : ''}`} onClick={toggleMenu}></div>}
+    </>
   );
 };
 
